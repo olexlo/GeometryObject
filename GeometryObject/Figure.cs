23d29490db8c1ec919f4b6f0bbddfd8736ee31f5
olexlo <olexiylo@gmail.com>
2023-10-22 19:26:46 +0300

    Add project files.

---
+++ GeometryObject/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryObject
+{
+    internal class Figure
+    {
+    }
+    internal class Line : Figure 
+    {
+    }
+    internal class Circle : Line
+    {
+    }
+    internal class Parallelepiped : Line 
+    {
+    }
+}
