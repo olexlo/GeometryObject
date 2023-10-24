adb205bc2fbbfba98f69f9b873da380e47a76e14
olexlo <olexiylo@gmail.com>
2023-10-24 11:50:14 +0300

    Step_9

--- GeometryObject/Figure.cs
+++ GeometryObject/Figure.cs
@@ -10,7 +10,11 @@ namespace GeometryObject
     {
         public string Name_Figure { get; set; }
         public int AmoutSides { get; set; }
-        
+        /// <summary>
+        /// name - Назва фігури, amout - Кількість унікальних сторін
+        /// </summary>
+        /// <param name="name_Figure"> Назва фігури</param>
+        /// <param name="amoutSides"> Кількість унікальних сторін</param>
         public Figure (string name_Figure, int amoutSides)
         {
             Name_Figure = name_Figure;
@@ -19,17 +23,15 @@ namespace GeometryObject
     }
     internal class Polygon : Figure
     {
+        
         public double[] Sides { get; set; }
-        public Polygon (int numberOfSides)
+        
+        public Polygon(string name, int numberOfSides):base(name, numberOfSides)
         {
             Sides = new double[numberOfSides];
-        }
-
-        public void SetSides()
-        {
-            for (int i = 0; i < Sides.Length; i++)
+            for ( int i = 0; i < Sides.Length; i++)
             {
-                Console.Write($"Введіть довжину сторони {i + 1}: ");
+                Console.Write($"Введіть довжину сторони {i + 1} {name}: ");
                 Sides[i] = Convert.ToDouble(Console.ReadLine());
             }
         }
@@ -41,6 +43,11 @@ namespace GeometryObject
         public static double Perimeter(double a, double b, double c)
         {
             return a + b + c;
+        }
+
+        public void PrintFigure(string math_operation,double result) 
+        {
+            Console.WriteLine($"Результат математичної {math_operation} {Name_Figure} буде дорівнювати {result}");
         }
     }
     internal class Radius : Figure 
@@ -55,17 +62,22 @@ namespace GeometryObject
     }
     internal class Parallelepiped : Polygon 
     {
-        public double[] Sides { get; set; }
-        public static double Perimeter(Sides)
+        //public double[] Sides { get; set; }
+        public Parallelepiped():base("Parallelepiped",2) 
         {
-            double a;
+        }        
+        public double Perimeter()
+        {
+            double a=0;
             for (int i = 0; i < Sides.Length; i++)
             {
                 a =+ Sides[i];
             }
             return a;
         }
-
+    }
+    internal class VolumeParallel : Parallelepiped 
+    {
     }
     internal class Square : Parallelepiped 
     {
--- GeometryObject/Program.cs
+++ GeometryObject/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var a = new Figure("Cu",5);
+            Parallelepiped parallelepiped = new Parallelepiped();
+            parallelepiped.PrintFigure("Периметр",parallelepiped.Perimeter());
         }
     }
 }
